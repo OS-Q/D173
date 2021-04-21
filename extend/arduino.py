37c27d2e32a130112633672900241a422b28fe20
qitas <qitas@qitas.cn>
2021-04-21 18:56:54 +0800

    fix

---
+++ extend/arduino.py
@@ -0,0 +1,95 @@
+from os.path import isdir, join
+
+from SCons.Script import DefaultEnvironment
+
+env = DefaultEnvironment()
+platform = env.PioPlatform()
+
+FRAMEWORK_DIR = platform.get_package_dir("framework-energia-ticc13x0")
+FRAMEWORK_VERSION = platform.get_package_version("framework-energia-ticc13x0")
+assert isdir(FRAMEWORK_DIR)
+
+
+env.Append(
+    CPPDEFINES=[
+        ("ARDUINO", 10811),
+        ("ENERGIA", 10811),
+        ("DeviceFamily_CC13X0"),
+        ("xdc_target_types__", "gnu/targets/arm/std.h"),
+        ("xdc_target_name__", "M3"),
+        ("xdc_cfg__header__", "configPkg/package/cfg/energia_pm3g.h"),
+        ("xdc__nolocalstring", "1"),
+        ("CORE_VERSION", "5200")
+    ],
+
+    CCFLAGS=[
+        "-mfloat-abi=soft",
+        "-mabi=aapcs"
+    ],
+
+    LINKFLAGS=[
+        "-Wl,-u,main",
+        "-Wl,--check-sections",
+        "-Wl,--gc-sections",
+    ],
+
+    LIBS=[":driverlib.lib"],
+
+    CPPPATH=[
+        join(FRAMEWORK_DIR, "system", "energia"),
+        join(FRAMEWORK_DIR, "system", "source"),
+        join(FRAMEWORK_DIR, "system", "source", "ti", "devices", "cc13x0"),
+        join(FRAMEWORK_DIR, "system", "source", "ti", "devices", "cc13x0", "inc"),
+        join(FRAMEWORK_DIR, "system", "source", "ti", "devices", "cc13x0", "driverlib"),
+        join(FRAMEWORK_DIR, "system", "kernel", "tirtos", "packages", "ti", "sysbios", "posix"),
+        join(FRAMEWORK_DIR, "system", "kernel", "tirtos", "packages"),
+        join(FRAMEWORK_DIR, "cores", env.BoardConfig().get("build.core")),
+        join(FRAMEWORK_DIR, "cores", env.BoardConfig().get("build.core"), "ti", "runtime", "wiring"),
+        join(FRAMEWORK_DIR, "cores", env.BoardConfig().get("build.core"), "ti", "runtime", "wiring", "cc13xx"),
+        join(FRAMEWORK_DIR, "variants", env.BoardConfig().get("build.variant"))
+    ],
+
+    LIBPATH=[
+        join(FRAMEWORK_DIR, "system", "energia"),
+        join(FRAMEWORK_DIR, "system", "source"),
+        join(FRAMEWORK_DIR, "system", "kernel"),
+        join(FRAMEWORK_DIR, "system", "kernel", "tirtos", "packages", "gnu", "targets", "arm", "libs",
+                            "install-native", "arm-none-eabi", "lib", "thumb", "v7-m"),
+        join(FRAMEWORK_DIR, "system", "kernel", "tirtos", "packages"),
+        join(FRAMEWORK_DIR, "system", "kernel", "tirtos", "builds", env.BoardConfig().get("build.variant"), "energia"),
+        join(FRAMEWORK_DIR, "system", "source", "ti", "devices", "cc13x0", "driverlib"),
+        join(FRAMEWORK_DIR, "system", "source", "ti", "devices", "cc13x0", "driverlib", "bin", "gcc"),
+        join(FRAMEWORK_DIR, "cores", env.BoardConfig().get("build.core"), "ti", "runtime", "wiring", "cc13xx"),
+        #join(FRAMEWORK_DIR, "cores", env.BoardConfig().get("build.core"), "ti", "runtime", "wiring", "cc13xx", "variants", "CC1310_LAUNCHXL"),
+        join(FRAMEWORK_DIR, "cores", env.BoardConfig().get("build.core"), "ti", "runtime", "wiring", "cc13xx", "variants", env.BoardConfig().get("build.variant")),
+        join(FRAMEWORK_DIR, "cores", env.BoardConfig().get("build.core")),
+    ],
+
+    # Using LDSCRIPT_PATH does not work as it uses gcc option "-T".
+    # We have to put linker command file within the linker start-group/end-group section
+    # to resolve all dependencies which are introduced by the linker command file
+    _LIBFLAGS=" -Wl,-T" + join(FRAMEWORK_DIR, "system", "energia", "linker.cmd") + " ",
+
+    LIBSOURCE_DIRS=[
+        join(FRAMEWORK_DIR, "libraries")
+    ],
+)
+
+#
+# Target: Build static core library and static variant library
+#
+
+libs = []
+
+libs.append(env.BuildLibrary(
+    join("$BUILD_DIR", "FrameworkEnergia"),
+    join(FRAMEWORK_DIR, "cores", env.BoardConfig().get("build.core"))
+))
+
+libs.append(env.BuildLibrary(
+    join("$BUILD_DIR", "FrameworkEnergiaVariant"),
+    join(FRAMEWORK_DIR, "variants", env.BoardConfig().get("build.variant"))
+))
+
+env.Append(LIBS=libs)
+
